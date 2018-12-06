c2b4fd10319118186f981a1c945161737ddf98dd
Mateusz Górniak <mateusz.gorniak@netguru.pl>
2018-12-06 10:22:25 +0100

    Define basic data about production server - host, port and SSL
    [#1]

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :codestories_review_bot, CodestoriesReviewBotWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "codestories-review-bot.herokuapp.com", port: System.get_env("PORT")],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
