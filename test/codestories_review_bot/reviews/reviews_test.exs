9d05a6164365bb9d50b41bef4e8222166a6c007a
Mateusz Górniak <mateusz.gorniak@netguru.pl>
2018-12-07 15:05:06 +0100

    Remove fixtures from ReviewsTest
    [#3]

--- test/codestories_review_bot/reviews/reviews_test.exs
+++ test/codestories_review_bot/reviews/reviews_test.exs
@@ -10,22 +10,15 @@ defmodule CodestoriesReviewBot.ReviewsTest do
     @update_attrs %{name: "some updated name"}
     @invalid_attrs %{name: nil}
 
-    def category_fixture(attrs \\ %{}) do
-      {:ok, category} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Reviews.create_category()
-
-      category
+    setup do
+      %{category: insert(:category)}
     end
 
-    test "list_categories/0 returns all categories" do
-      category = category_fixture()
+    test "list_categories/0 returns all categories", %{category: category} do
       assert Reviews.list_categories() == [category]
     end
 
-    test "get_category!/1 returns the category with given id" do
-      category = category_fixture()
+    test "get_category!/1 returns the category with given id", %{category: category} do
       assert Reviews.get_category!(category.id) == category
     end
 
@@ -38,26 +31,22 @@ defmodule CodestoriesReviewBot.ReviewsTest do
       assert {:error, %Ecto.Changeset{}} = Reviews.create_category(@invalid_attrs)
     end
 
-    test "update_category/2 with valid data updates the category" do
-      category = category_fixture()
+    test "update_category/2 with valid data updates the category", %{category: category} do
       assert {:ok, %Category{} = category} = Reviews.update_category(category, @update_attrs)
       assert category.name == "some updated name"
     end
 
-    test "update_category/2 with invalid data returns error changeset" do
-      category = category_fixture()
+    test "update_category/2 with invalid data returns error changeset", %{category: category} do
       assert {:error, %Ecto.Changeset{}} = Reviews.update_category(category, @invalid_attrs)
       assert category == Reviews.get_category!(category.id)
     end
 
-    test "delete_category/1 deletes the category" do
-      category = category_fixture()
+    test "delete_category/1 deletes the category", %{category: category} do
       assert {:ok, %Category{}} = Reviews.delete_category(category)
       assert_raise Ecto.NoResultsError, fn -> Reviews.get_category!(category.id) end
     end
 
-    test "change_category/1 returns a category changeset" do
-      category = category_fixture()
+    test "change_category/1 returns a category changeset", %{category: category} do
       assert %Ecto.Changeset{} = Reviews.change_category(category)
     end
   end
