cc641d8811a0f9ad23c2e2339ba168aa3a3a10b7
Mateusz Górniak <mateusz.gorniak@netguru.pl>
2018-12-07 16:41:15 +0100

    Remove all fixtures
    [#3]

--- test/codestories_review_bot_web/controllers/category_controller_test.exs
+++ test/codestories_review_bot_web/controllers/category_controller_test.exs
@@ -1,8 +1,6 @@
 defmodule CodestoriesReviewBotWeb.CategoryControllerTest do
   use CodestoriesReviewBotWeb.ConnCase
 
-  alias CodestoriesReviewBot.Reviews
-
   setup %{conn: conn} do
     %{
       category: insert(:category, name: "test"),
@@ -11,7 +9,7 @@ defmodule CodestoriesReviewBotWeb.CategoryControllerTest do
   end
 
   describe "index" do
-    test "lists all categories", %{category: category, conn: conn} do
+    test "lists all categories", %{conn: conn} do
       conn = get(conn, Routes.category_path(conn, :index))
       assert [%{"id" => _, "name" => "test"}] = json_response(conn, 200)["data"]
     end
--- test/codestories_review_bot_web/controllers/reviewer_controller_test.exs
+++ test/codestories_review_bot_web/controllers/reviewer_controller_test.exs
@@ -1,48 +1,42 @@
 defmodule CodestoriesReviewBotWeb.ReviewerControllerTest do
   use CodestoriesReviewBotWeb.ConnCase
 
-  alias CodestoriesReviewBot.Reviews
-
-  setup do
-    %{category: insert(:category)}
-  end
-
-  @create_attrs %{
-    category_id: 1, slack_id: "some slack_id"
-  }
-  @invalid_attrs %{slack_id: nil}
-
-  def fixture(:reviewer) do
-    {:ok, reviewer} = Reviews.create_reviewer(@create_attrs)
-    reviewer
-  end
-
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    category = insert(:category)
+    reviewer = insert(:reviewer, category: category)
+
+    %{
+      category: category,
+      conn: put_req_header(conn, "accept", "application/json"),
+      reviewer: reviewer
+    }
   end
 
   describe "index" do
     test "lists all reviewers", %{conn: conn} do
       conn = get(conn, Routes.reviewer_path(conn, :index))
-      assert json_response(conn, 200)["data"] == []
+      assert [%{"id" => _, "category_id" => _, "slack_id" => _}] = json_response(conn, 200)["data"]
     end
   end
 
   describe "create reviewer" do
-    test "renders reviewer when data is valid", %{conn: conn, category: category} do
-      conn = post(conn, Routes.reviewer_path(conn, :create), reviewer: %{slack_id: "some_slack_id", category_id: category.id})
-      assert %{"id" => id} = json_response(conn, 201)["data"]
+    test "renders reviewer when data is valid", %{category: category, conn: conn} do
+      conn = post(conn,
+                  Routes.reviewer_path(conn, :create),
+                  reviewer: %{category_id: category.id, slack_id: "some slack_id"})
+      assert %{"id" => _, "category_id" => _, "slack_id" => _} = json_response(conn, 201)["data"]
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.reviewer_path(conn, :create), reviewer: @invalid_attrs)
+      conn = post(conn,
+                  Routes.reviewer_path(conn, :create),
+                  reviewer: %{slack_id: "some slack_id"})
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
 
   describe "delete reviewer" do
-    test "deletes chosen reviewer", %{conn: conn, category: category} do
-      reviewer = insert(:reviewer, category: category)
+    test "deletes chosen reviewer", %{conn: conn, reviewer: reviewer} do
       conn = delete(conn, Routes.reviewer_path(conn, :delete, reviewer))
       assert response(conn, 204)
     end
